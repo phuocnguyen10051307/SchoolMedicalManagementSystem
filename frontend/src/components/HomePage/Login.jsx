05567a52c98b0f39916f56ed56220503308c8d08
phuocnguye10051307 <phuocmoi4321@gmail.com>
2025-06-19 20:56:34 +0700

    fix code login use be and fe

--- frontend/src/components/HomePage/Login.jsx
+++ frontend/src/components/HomePage/Login.jsx
@@ -5,17 +5,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { toast } from "react-toastify";
 import "./login.css";
-
-
-// function App() {
-//   const [adminData, setAdminData] = useState(null);
-//   const [parentData, setParentData] = useState(null);
-//   const [studentData, setStudentData] = useState(null);
-//   const { setUser } = useContext(AuthContext);
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [errorMsg, setErrorMsg] = useState("");
-//   const navigate = useNavigate();
+import { loginAccount } from "../../service/service";
 
 const Login = () => {
   const [adminData, setAdminData] = useState(null);
@@ -29,95 +19,24 @@ const Login = () => {
   const [errorMsg, setErrorMsg] = useState("");
   const navigate = useNavigate();
 
-
-  useEffect(() => {
-    fetchData()
-      .then((data) => {
-        setAdminData(data.Admins);
-        setParentData(data.Parents);
-        setStudentData(data.Students);
-        setSchoolnurseData(data.SchoolNurses);
-        setManagerData(data.Managers);
-      })
-      .catch((error) => {
-        console.log(error);
-        setErrorMsg("Lỗi khi tải dữ liệu.");
-      });
-  }, []);
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
     setErrorMsg("");
-
-
-    if (!adminData) {
-      setErrorMsg("Dữ liệu admin chưa load xong, vui lòng đợi.");
-      return;
-    }
-    const checkAdmin = adminData.find((admin) => {
-      return admin.Username === username && admin.PasswordHash === password;
-    });
-
-    if (!parentData) {
-      setErrorMsg("Dữ liệu parents chưa load xong, vui lòng đợi.");
-      return;
-    }
-    const checkParent = parentData.find((parent) => {
-      return parent.Username === username && parent.PasswordHash === password;
-    });
-
-    if (!studentData) {
-      setErrorMsg("Dữ liệu students chưa load xong, vui lòng đợi.");
-      return;
-    }
-    const checkStudent = studentData.find((stu) => {
-      return stu.Username === username && stu.PasswordHash === password;
-    }); 
-
-    if (!schoolnurseData) {
-      setErrorMsg("Dữ liệu schoolnurse chưa load xong, vui lòng đợi.");
-      return;
-    }
-    const checkSchoolnurse = schoolnurseData.find((schoolnurse) => {
-      return schoolnurse.Username === username && schoolnurse.PasswordHash === password;
-    });
-
-    if (!managerData) {
-      setErrorMsg("Dữ liệu manager chưa load xong, vui lòng đợi.");
-      return;
-    }
-    const checkManager = managerData.find((manager) => {
-      return manager.Username === username && manager.PasswordHash === password;
-    });
-
-
-
-    if (checkAdmin) {
-      setUser(checkAdmin);
-      navigate("/admin");
-      return;
-    }
-    if (checkParent) {
-      setUser(checkParent);
-      navigate("/parent");
-      return;
-    }
-    if (checkStudent) {
-      setUser(checkStudent);
-      navigate("/student");
-      return;
-    }
-    if (checkSchoolnurse) {
-      setUser(checkSchoolnurse);
-      navigate("/schoolnurse");
-      return;
-    }
-    if (checkManager) {
-      setUser(checkManager);
-      navigate("/manager");
-      return;
-    } else {
-      console.log(checkAdmin);
-      toast.error("Tên đăng nhập hoặc mật khẩu không đúng");
+    const rows = await loginAccount(username,password)
+    const account = rows.account;
+    setUser(account)
+    switch(account.role){
+      case "ADMIN":
+        navigate("/admin")
+        break;
+      case "MANAGER":
+        navigate("/manager")
+        break;
+      case "PARENT":
+        navigate("/parent")
+        break;
+      default:
+        setErrorMsg("Failed username or password")
     }
   };
 
@@ -125,8 +44,6 @@ const Login = () => {
   return (
 
       <div className="login-page">
-
-
         <div
           className="login-container"
         // style={{ maxWidth: 400, margin: "auto", padding: 20 }}
