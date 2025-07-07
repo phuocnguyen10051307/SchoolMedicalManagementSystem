cda14d11a627f88d96f13b48458a5a83bd437b80
NgHan15 <hanltnse184409@fpt.edu.vn>
2025-07-07 07:49:56 +0700

    update api periodicNotifications

--- backend/src/services/eventQueries.js
+++ backend/src/services/eventQueries.js
@@ -33,20 +33,11 @@ const getPeriodicCheckupsByParentId = async (accountId) => {
   const client = await connection.connect();
   try {
     const { rows } = await client.query(
-      `SELECT 
-          me.event_id,
-          me.event_title,
-          me.event_datetime,
-          me.severity_level,
-          s.full_name AS student_name,
-          s.class_name
-    FROM medical_events me
-    JOIN students s ON me.student_id = s.student_id
-    JOIN parents p ON s.student_id = p.student_id
-    WHERE me.event_type = 'fever'
-      AND p.account_id = $1 
-    ORDER BY me.event_datetime DESC;
-    `,
+      `SELECT en.*, me.event_title, me.event_description, me.event_datetime, s.full_name AS student_name
+FROM event_notifications en
+JOIN medical_events me ON en.event_id = me.event_id
+JOIN students s ON me.student_id = s.student_id
+WHERE en.parent_account_id = $1`,
       [accountId]
     );
     return rows;
